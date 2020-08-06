9ab562493b3fb85f429fe0ac2e38cc5a073bc2f5
Tiunchik <senebh@gmail.com>
2020-08-06 22:35:16 +0300

    first commit

---
+++ src/main/java/org/bundle/Start.java
@@ -0,0 +1,4 @@
+package org.bundle;
+
+public class Start {
+}
---
+++ src/main/java/org/bundle/controllers/ReportController.java
@@ -0,0 +1,17 @@
+package org.bundle.comtrollers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ReportController {
+
+    @GetMapping("/report")
+    public ResponseEntity<String> getSpeakers(){
+        return new ResponseEntity<>("Done", HttpStatus.OK);
+    }
+
+
+}
